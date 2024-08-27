c95d2b9f7bef21384d34aae72c93b91ea14292fa
Dheeraj1479 <dheeraj14krishna@gmail.com>
2024-08-27 16:24:54 +0100

    day-10
    calculator

--- day-10.py
+++ day-10.py
@@ -1,15 +1,9 @@
 def add(n1, n2):
     return n1 + n2
-
-
 def subtract(n1, n2):
     return n1 - n2
-
-
 def multiply(n1, n2):
     return n1 * n2
-
-
 def divide(n1, n2):
     return n1 / n2
 
@@ -26,21 +20,21 @@ def calculator():
     while should_accumulate:
         for symbol in operations:
             print(symbol)
-            operation_symbol = input("Pick an operation:")
-            num2 = float(input("Enter second number: "))
-            answer = operations[operation_symbol](num1, num2)
-            print(f"{num1} {operation_symbol} {num2} = {answer}")
+        operation_symbol = input("Pick an operation:")
+        num2 = float(input("Enter second number: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-            choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to stop: ")
+        choice = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to stop: ")
 
-            if choice == "y":
-                num1 = answer
-            else:
-                should_accumulate = False
-                print("\n" * 20)
-                calculator()
+        if choice == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n" * 20)
+            calculator()
 
-
+calculator()
 
 
 
